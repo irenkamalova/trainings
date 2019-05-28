e4d71a2bcfd8dbcc8cb4db546c0eece8598052ab
irenkamalova <iren.kamalova@gmail.com>
2019-05-28 17:19:21 +0300

    Create FindModeInBinaryTree.java

---
+++ src/main/java/com/kamalova/leetcode/FindModeInBinaryTree.java
@@ -0,0 +1,36 @@
+package com.kamalova.leetcode;
+
+/*
+Easy task
+Apllyed from the first attempt
+*/
+
+class FindModeInBinaryTree {
+    Map<Integer, Integer> map = new HashMap<>();
+    int max = 0;
+    List<Integer> l = new ArrayList<>();
+    
+    public int[] findMode(TreeNode root) {
+        check(root);
+        for (Map.Entry<Integer, Integer> i : map.entrySet()) {
+            if (i.getValue() == max) {
+                l.add(i.getKey());
+            }
+        }
+        
+        int[] res = new int[l.size()];
+        for (int i = 0; i < l.size(); i++) {
+            res[i] = l.get(i);
+        }
+        return res;
+    }
+    
+    private void check(TreeNode node) {
+        if (node != null) {
+            map.put(node.val, map.getOrDefault(node.val, 0) + 1);
+            max = Math.max(map.get(node.val), max);
+            check(node.left);
+            check(node.right);
+        }
+    }
+}
