c95701ea61aa8a8beed02bf4739013485a0bd427
irenkamalova <iren.kamalova@gmail.com>
2019-06-01 21:07:39 +0300

    Update WraparoundString.java

--- src/main/java/com/kamalova/leetcode/WraparoundString.java
+++ src/main/java/com/kamalova/leetcode/WraparoundString.java
@@ -3,46 +3,60 @@ package com.kamalova.image;
 import java.util.*;
 
 /*
-First attempt - brute force solution (using substring brute method)
-(time limited exceed)
+Get (good) decision from discussion
+
+Success
+Details 
+Runtime: 6 ms, faster than 93.71% of Java online submissions for Unique Substrings in Wraparound String.
+Memory Usage: 35 MB, less than 98.75% of Java online submissions for Unique Substrings in Wraparound String.
+
 */
 class WraparoundString {
     public int findSubstringInWraproundString(String p) {
-
         int n = p.length();
-        Map<String, Boolean> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
+        int[] lenghts = new int[26];
 
-            for (int j = i + 1; j <= n; j++) {
-                if (!map.containsKey(p.substring(i, j))) {
-                    map.put(p.substring(i, j), isWrapAround(p.substring(i, j), map));
-                }
-                if (!map.containsKey(p.substring(j, n))) {
-                    map.put(p.substring(j, n), isWrapAround(p.substring(j, n), map));
-                }
+        int start;
+        int current = 0;
+        while (current < n) {
+            start = current;
+            while ((current < n - 1) && (getInt(p, current) + 1) % 26 == getInt(p, current + 1)) {
+                current++;
             }
+
+            for (int i = start; i <= current; i++) {
+                int numberOfAppearances = current - i + 1; // 1 - about letter exactly
+                if (numberOfAppearances <= lenghts[getInt(p, i)]) {
+                    break;
+                }
+                lenghts[getInt(p, i)] = numberOfAppearances;
+            }
+            current++;
         }
+
         int res = 0;
-        for (Boolean b : map.values()) {
-            if (b) res++;
+        for (int i : lenghts) {
+            res += i;
         }
         return res;
     }
 
-    private boolean isWrapAround(String p) {
-        if (p.length() == 0) return false;
-        for (int i = 0; i < p.length() - 1; i++) {
-            if ( ((p.charAt(i) - 'a') + 1) % 26 != (p.charAt(i + 1) - 'a')) return false;
-            // a = 0; a -> next = a + 1 = 1, 1 % 26 = 1 == 'b' -> 'ab' -> true
-            // z = 25, z -> next = z + 1 = 26, 26 % 26 == 0 == 'a' -> 'za' -> true
-        }
-        return true;
+    private int getInt(String p, int index) {
+        return p.charAt(index) - 'a';
     }
-    
+
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.findSubstringInWraproundString("zab"));
+        System.out.println(s.findSubstringInWraproundString("erflokatkn"));
         int a = s.findSubstringInWraproundString("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz");
-        System.out.println(a); // 34151
+        System.out.println(a);
     }
+
+    /*
+    For debugging:
+    
+    a b c d e f g h i j k  l  m  n  o   p   q   r   s  t  u  v   w   x  y   z
+    0 1 2 3 4 5 6 7 8 9 10 11 12 13 14  15  16  17 18 19 20 21  22  23 24  25
+
+     */
 }
