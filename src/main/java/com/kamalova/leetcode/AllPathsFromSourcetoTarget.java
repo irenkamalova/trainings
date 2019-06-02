06881b3fb23b0bf2a5234eed326c43752c67a856
irenkamalova <iren.kamalova@gmail.com>
2019-06-02 13:03:30 +0300

    Update AllPathsFromSourcetoTarget.java

--- src/main/java/com/kamalova/leetcode/AllPathsFromSourcetoTarget.java
+++ src/main/java/com/kamalova/leetcode/AllPathsFromSourcetoTarget.java
@@ -5,32 +5,28 @@ import java.util.*;
 /*
 Success
 Details 
-Runtime: 4 ms, faster than 42.67% of Java online submissions for All Paths From Source to Target.
-Memory Usage: 39 MB, less than 99.93% of Java online submissions for All Paths From Source to Target.
+Runtime: 2 ms, faster than 99.28% of Java online submissions for All Paths From Source to Target.
+Memory Usage: 41.3 MB, less than 84.91% of Java online submissions for All Paths From Source to Target.
 */
 
 class AllPathsFromSourcetoTarget {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        
         List<Integer> l = new ArrayList<>();
         l.add(0);
-        return dfs(graph, graph[0], l);
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(graph, graph[0], 0, l, res);
+        return res;
     }
     
-    List<List<Integer>> dfs(int[][] graph, int[] nodes, List<Integer> l) {
-        List<List<Integer>> res = new ArrayList<>();
-        for (int node : nodes) {
-            List<Integer> nL = new ArrayList<>();
-            nL.addAll(l);
-            nL.add(node);
-            List<List<Integer>> res2 = dfs(graph, graph[node], nL);
-            if (res2.isEmpty()) {
-            res.add(nL);
-            } else {
-            res.addAll(res2);
-            }
+    void dfs(int[][] graph, int[] nodes, int node, List<Integer> l, List<List<Integer>> res) {
+        if (node == graph.length - 1) {
+            res.add(new ArrayList<>(l));
         }
         
-        return res;
+        for (int nod : nodes) {
+            l.add(nod);
+            dfs(graph, graph[nod], nod, l, res);
+            l.remove(l.size() - 1);
+        }
     }
 }
